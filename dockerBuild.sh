5e20d123f56ac0fdfd93cebfe24df959b9adae4c
pabloqr <pabloquesadarojo02@gmail.com>
2023-10-27 11:50:49 +0200

    added commands to script

--- dockerBuild.sh
+++ dockerBuild.sh
@@ -3,3 +3,5 @@ docker run -p 8080:8080 -v "$(pwd)/appdata:/app" -it -d --name appcesible appces
 
 #   start with:
 #   docker start appcesible
+#	docker exec -it appcesible /bin/bash
+#	docker stop appcesible
