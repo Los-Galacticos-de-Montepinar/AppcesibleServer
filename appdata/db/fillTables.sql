83c014a8111ce5fb85ad968ba03e91b5f4261be1
pabloqr <pabloquesadarojo02@gmail.com>
2023-12-07 13:06:34 +0100

    new entries for fillTables

--- appdata/db/fillTables.sql
+++ appdata/db/fillTables.sql
@@ -2,9 +2,16 @@
 DELETE FROM user;
 DELETE FROM student;
 DELETE FROM loginInfo;
+DELETE FROM item;
 
 INSERT INTO user (id,userName,idProfileImg,userType,idClass) VALUES (NULL,'Johnny Joestar',2,1,0);
 INSERT INTO user (id,userName,idProfileImg,userType,idClass) VALUES (NULL,'Gyro Zeppeli',3,0,0);
 INSERT INTO loginInfo (id,idUser,method,textPass,passPart0,passPart1,passPart2) VALUES (NULL,1,1,NULL,'img','img','img');
 INSERT INTO loginInfo (id,idUser,method,textPass,passPart0,passPart1,passPart2) VALUES (NULL,2,0,'pass33',NULL,NULL,NULL);
 INSERT INTO student (id,idUser,letterSize,interactionFormat) VALUES (NULL,1,20,1);
+
+INSERT INTO item (id, itemName, imageName) VALUES (NULL,'Lápiz','img');
+INSERT INTO item (id, itemName, imageName) VALUES (NULL,'Boli','img');
+INSERT INTO item (id, itemName, imageName) VALUES (NULL,'Regla','img');
+INSERT INTO item (id, itemName, imageName) VALUES (NULL,'Goma','img');
+INSERT INTO item (id, itemName, imageName) VALUES (NULL,'Hoja','img');
